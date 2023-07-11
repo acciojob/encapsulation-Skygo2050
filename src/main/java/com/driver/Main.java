d11f15a8a02720522c1406692b33750299c4f52e
skygo <aakashjha2050@gmail.com>
2023-07-11 16:43:08 +0530

    Don21

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,22 @@
 package com.driver;
 
 public class Main {
-  
+
+
+
+
+
+
+    public static void main(String[] args) {
+
+
+        RWOnly obj = new RWOnly();
+
+
+      obj.setName("Aakash");
+
+      obj.getName();
+    }
+
+
 }
