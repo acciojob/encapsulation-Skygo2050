fc7ff8cebecb2ac0d9f6bdc533f4e8c055fb8c46
skygo <aakashjha2050@gmail.com>
2023-07-11 20:36:35 +0530

    Don21

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,8 +1,11 @@
 package com.driver;
 
+
 public class Main {
-
-
-
-
+    // we can't access any object of class in main so we have to create psvm to access
+    public static void main(String[] args) {
+        RWOnly Rw = new RWOnly();
+        Rw.setName("Aakash");
+        System.out.println(Rw.getName());
+    }
 }
